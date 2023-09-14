8f0df9c7584e865178cdf8e8aa74f831400e3554
Cyadine <61088560+zvbt@users.noreply.github.com>
2023-09-14 22:58:07 +0200

    Create mdl.js

---
+++ assets/js/mdl.js
@@ -0,0 +1,6 @@
+var wc = "<div className=\"mdlw-container-X2LPjg\">\n\t\n\n\t\n\n\t\n\t<div className=\"mdlw-item-X2LPjg\">\n\t\t\n\t\t<div className=\"mdlw-cover-X2LPjg\"><a href=\"https://mydramalist.com/61489-destruction?utm_medium=widgets&amp;utm_source=list_updates\" title=\"Doom at Your Service\"><img src=\"https://i.mydramalist.com/vlNOX_4t.jpg\"></a></div>\n\t\t\n\t\t<div className=\"mdlw-content-X2LPjg\">\n\t\t\t\n\t\t\t<div className=\"mdlw-title-X2LPjg\"><a href=\"https://mydramalist.com/61489-destruction?utm_medium=widgets&amp;utm_source=list_updates\" title=\"Doom at Your Service\">Doom at Your Service</a> <span className=\"mdlw-ep-seen\">(0/16)</span></div>\n\t\t\t\n\t\t\t\n\t\t\t\n\t\t\t<div className=\"mdlw-status-X2LPjg\">\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t<div className=\"mdlw-time-X2LPjg\">16 minutes ago</div>\n\t\t\t\t\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\t\n\n\t\n\t</div>"
+
+var wdiv = document.getElementById('mdl_widget_X2LPjg')
+if (wdiv) {
+	wdiv.innerHTML = wc
+}
